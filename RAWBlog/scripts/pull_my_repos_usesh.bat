ad3f8cf02d1fce9d0dfb97f75fffd27b59e39ead
CHQS407 <aucnm0202@163.com>
2025-12-02 16:00:27 +0800

    更新 2025-12-02 16:00:23

--- RAWBlog/scripts/pull_my_repos_usesh.bat
+++ RAWBlog/scripts/pull_my_repos_usesh.bat
@@ -1,9 +1,11 @@
 @echo off
+echo %date% %time%: 脚本已启动 >> C:\Logs\bat_log.txt
+chcp 65001
 setlocal
 
-:: 使用正确的Git Bash解释器路径
+
 set "GIT_BASH=D:\Program Files\Git\bin\bash.exe"
-:: 使用Git Bash可识别的Unix风格路径
+
 set "SCRIPT=/d/MyBlog/RAWBlog/scripts/pull_my_repos.sh"
 
 echo 调用 Git Bash 执行脚本...
